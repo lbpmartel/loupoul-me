e29c16addd9b8cee8aa0f392b19fce07ddbc75d7
lbpmartel <lbpmartel@gmail.com>
2021-07-08 11:58:10 -0400

    started pagination

---
+++ src/templates/allPosts.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import { graphql } from "gatsby"
+
+import { Container, Content, BlogPreviewCard, Pagination } from "../components"
+
+import { H1, P } from "../elements"
+
+const allPosts = ({pageContext})
+
