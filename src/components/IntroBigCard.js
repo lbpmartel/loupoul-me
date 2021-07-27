712b16bd8285a7633adabe50c62835bce366f4d3
Louis-Bernard Poulin Martel <lbpmartel@gmail.com>
2021-07-26 21:41:11 -0400

    Added Github link to Intro

--- src/components/IntroBigCard.js
+++ src/components/IntroBigCard.js
@@ -16,6 +16,7 @@ export const IntroBigCard = () => {
             <br />
             <br />
             Bienvenue sur ma petite landing page! Elle m'a permis d'expérimenter avec le framework <A href="https://www.gatsbyjs.com/"> Gatsby</A>. Comme elle a été faite par temps libres, elle se veut sans artifice ni fla-fla...
+            Si ça vous intéresse, voici le <A href="https://github.com/lbpmartel/loupoul-me">dépôt Github.</A>
             <br />
             <br />
             Je travaille avec <A href="https://reactjs.org/">React</A> depuis maintenant { nbYearsExpFE } ans et je suis dans le développement logiciel depuis 2012.
